fcc9d81589fd9b6633be38cb915b5f223c6e51c5
Kelvinskell <Kelvinskelll@gmail.com>
2021-11-22 01:38:12 +0100

    Initial commit

---
+++ lowercase.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#The function of this script is to change filenames to lowercase characters for easier terminal use.
+#This script is specially adapted for Linux systems only and may not (properly) run on Windows or other Operating Systems.
+trap func exit
+function func()
+{
+	rm error_log.txt 2>/dev/null
+}
+
+function F1()
+{
+var1=`ls`
+for i in $var1
+do
+	mv -v $i `tr '[:upper:]' '[:lower:]' < <(echo "$i")` 2>error_log.txt
+	logger $0: `cat error_log.txt`
+done
+if [ $? == 0 ]
+then
+	echo "TASK COMPLETED!"
+else
+	echo "Error code $?, Please use journalctl for more diagnosis"
+fi
+}
+
+function F2()
+{
+	echo "Please enter the ABSOLUTE Directory path for the files(e.g /home/$USER/Videos):"
+	read path
+	cd $path 2>/dev/null
+	if [ -d $path ]
+	then
+	for i in `ls -R`
+	do
+		mv -v $i "$path/`tr '[:upper:]' '[:lower:]' < <(echo "$i")`" 2>error_log.txt
+		logger $0: `cat error_log.txt`
+	done
+	echo "TASK COMPLETED!"
+else 
+	echo -e "Path does not exist! "
+	fi
+}
+
+while getopts "dDrR" options
+do
+	case ${options} in
+		d | D)
+			F1
+			;;
+		r | R)
+			F2
+			;;
+		*)
+			echo "Use -d or -D option for files in the current directory only, Use -r or -R option to specify a directory and peform action on the files therein."
+
+		esac
+	done
+	#This Program was written by kelvin C Onuchukwu
+	#https://www.linkedin.com/in/kelvin-onuchukwu-3460871a1
+	#Please report any bugs to kelvinskelll@gmail.com
